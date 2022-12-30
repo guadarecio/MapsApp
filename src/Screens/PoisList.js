834573e228089219941a8e0a9fbbd42702bc9b3a
Guadalupe Recio <guadaluperecio@gmail.com>
2022-12-30 14:21:56 +0100

    first

--- src/Screens/PoisList.js
+++ src/Screens/PoisList.js
@@ -1,43 +1,21 @@
 import React, {useContext} from 'react';
 import {View, Text, TouchableOpacity, FlatList, Image} from 'react-native';
 import {DataContext} from '../Context/DataContext';
+import MapView from 'react-native-maps';
 
 const PoisList = ({navigation}) => {
   const pois = useContext(DataContext);
 
   return (
     <View style={{flex: 1}}>
-      <Text>Pois List</Text>
-
-      <FlatList
-        data={pois}
-        renderItem={({item}) => {
-          return (
-            <View
-              style={{
-                justifyContent: 'flex-start',
-                alignItems: 'center',
-                flexDirection: 'row',
-              }}>
-              <Image
-                source={{
-                  uri: item.image.url,
-                }}
-                resizeMode={'cover'}
-                style={{
-                  width: 100,
-                  height: 100,
-                }}
-              />
-              <Text>{item.name}</Text>
-            </View>
-          );
+      <MapView
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
         }}
       />
-
-      <TouchableOpacity onPress={() => navigation.navigate('PoisMap')}>
-        <Text>MOSTRAR EN MAPA</Text>
-      </TouchableOpacity>
     </View>
   );
 };
